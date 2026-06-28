69a12c36ca4c0a6e9d729c0018bee5b26d7ef1fc
Will Massam <161020350+wmas516@users.noreply.github.com>
2026-06-28 15:01:30 +1200

    feat: added cleanable boot minigame (#14)
    * feat: added cleanable boot minigame
    
    - User can click and drag over myrtle spots to reduce transparency and remove it over time.
    - Once all myrtle cleared minigame will end
    
    * chore: relocated files and changed console logging format

---
+++ guardians-of-the-forrest/main/scripts/src/main/minigames/CleaningBoot.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+@export var sprites: Array[Sprite2D]
+
+var isDragging = false
+
+func _ready():
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				var curSprite = isPointInsideSprite(event.global_position)
+				isDragging = true
+				if curSprite:
+					dragSprite(curSprite)
+			else:
+				isDragging = false
+
+	if event is InputEventMouseMotion && isDragging:
+		var curSprite = isPointInsideSprite(event.global_position)
+		if curSprite:
+			dragSprite(curSprite)
+
+func dragSprite(curSprite):
+	if curSprite.modulate.a > 0.1:
+		curSprite.modulate.a = curSprite.modulate.a - 0.01
+	else: 
+		curSprite.modulate.a = 0
+		sprites.erase(curSprite)
+		curSprite.queue_free()
+		if (sprites.size() <= 0):
+			miniGameOver()
+		else:
+			clearedSprite(sprites.size())
+
+func isPointInsideSprite(globalPoint: Vector2) -> Sprite2D:
+	if sprites == null:
+		return null 
+	
+	for curSprite in sprites:
+		var curSpriteRect: Rect2
+		var localPos = curSprite.to_local(globalPoint)
+		var textureSize = curSprite.texture.get_size()
+		
+		if curSprite.centered:
+			curSpriteRect = Rect2(-textureSize / 2, textureSize)
+		else:
+			curSpriteRect = Rect2(Vector2.ZERO, textureSize)
+		if (curSpriteRect.has_point(localPos)):
+			return curSprite
+	return null
+	
+func clearedSprite(left: int):
+	print("[Removed Disease]:")
+	print(" - ", left , " left to go")
+func miniGameOver():
+	print("Boot Cleared")
