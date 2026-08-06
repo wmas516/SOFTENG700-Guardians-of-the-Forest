2a6900eb81ac3bf8a8d848de1af629a976a8033f
Will Massam <wmas516@aucklanduni.ac.nz>
2026-08-06 19:26:02 +1200

    feat: integrated boss level into platforming

--- guardians-of-the-forrest/main/scripts/src/main/defense/Main.gd
+++ guardians-of-the-forrest/main/scripts/src/main/defense/Main.gd
@@ -1,44 +1,6 @@
 extends Node2D
 
-class Wave:
-	var normal: int
-	var elite: int
-	var boss: int
-	func _init(pNormal: int, pElite: int, pBoss: int):
-		normal = pNormal
-		elite = pElite
-		boss = pBoss
-	func spawnNormal() -> void:
-		normal -= 1
-	func spawnElite() -> void:
-		elite -= 1
-	func spawnBoss() -> void:
-		boss -= 1
-	func getRandEnemyBossLast() -> DefenceEnemy.EnemyType:
-		if (total() > 0):
-			var options = []
-			if (normal > 0):
-				options.append(DefenceEnemy.EnemyType.NORMAL)
-			if (elite > 0):
-				options.append(DefenceEnemy.EnemyType.ELITE)
-			if (options.is_empty() && boss > 0):
-				options.append(DefenceEnemy.EnemyType.BOSS)
-			var index = randi() % options.size()
-			subType(options[index])
-			return (options[index])
-		return (DefenceEnemy.EnemyType.NORMAL)
-	func subType(type:DefenceEnemy.EnemyType):
-		match type:
-			DefenceEnemy.EnemyType.NORMAL:
-				spawnNormal()
-			DefenceEnemy.EnemyType.ELITE:
-				spawnElite()
-			DefenceEnemy.EnemyType.BOSS:
-				spawnBoss()
-	func total() -> int:
-		return (normal + elite + boss)
-
-@export var waveEnemies: Array = [Wave.new(1,1,1),Wave.new(2,1,0),Wave.new(0,0,2),Wave.new(3,2,1)]
+@export var waveEnemies: Array[Wave] = [Wave.new(1,1,1),Wave.new(2,1,0),Wave.new(0,0,2),Wave.new(3,2,1)]
 @export var wave = 0
 @export var aliveEnemies = 0
 @export var enemy: CharacterBody2D
