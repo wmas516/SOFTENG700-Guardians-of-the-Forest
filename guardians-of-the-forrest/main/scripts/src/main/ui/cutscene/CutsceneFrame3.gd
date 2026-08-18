a9552d8ab6c1fb16f52c7ffea009c8acce70491b
Will Massam <wmas516@aucklanduni.ac.nz>
2026-08-18 16:35:41 +1200

    feat: Added captions and background to cutscene
    - Added timed caption class that specifies a caption's text and how long its on the screen for
    - Added backgrounds without foreground elements

--- guardians-of-the-forrest/main/scripts/src/main/ui/cutscene/CutsceneFrame3.gd
+++ guardians-of-the-forrest/main/scripts/src/main/ui/cutscene/CutsceneFrame3.gd
@@ -1,34 +1,9 @@
-extends Control
+extends CutsceneFrame
 
 @onready var forest: TextureRect = $Forest
 @onready var dirt: TextureRect = $Dirt
 @onready var bush: TextureRect = $Bush
 @onready var kea: TextureRect = $Kea
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	if (Input.is_action_pressed("Left")):
-		parallaxLeft()
-	
-	elif (Input.is_action_pressed("Right")):
-		parallaxRight()
-	
-	pass
-
-func parallaxLeft():
-	parallax(Vector2(-0.5, 0))
-	return
-
-func parallaxRight():
-	parallax(Vector2(0.5, 0))
-	return
-	
 
 func parallax(dir: Vector2):
 	forest.set_position(forest.position + 0.25 * dir)
--- guardians-of-the-forrest/main/scripts/src/main/ui/cutscene/CutsceneFrame4.gd
+++ guardians-of-the-forrest/main/scripts/src/main/ui/cutscene/CutsceneFrame4.gd
@@ -1,35 +1,10 @@
-extends Control
+extends CutsceneFrame
 
 @onready var forest: TextureRect = $Forest
 @onready var dirt: TextureRect = $Dirt
 @onready var bush: TextureRect = $Bush
 @onready var hills: TextureRect = $Hills
 @onready var sky: TextureRect = $Sky
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	if (Input.is_action_pressed("Left")):
-		parallaxLeft()
-	
-	elif (Input.is_action_pressed("Right")):
-		parallaxRight()
-	
-	pass
-
-func parallaxLeft():
-	parallax(Vector2(-0.5, 0))
-	return
-
-func parallaxRight():
-	parallax(Vector2(0.5, 0))
-	return
-	
 
 func parallax(dir: Vector2):
 	forest.set_position(forest.position + 0.25 * dir)
