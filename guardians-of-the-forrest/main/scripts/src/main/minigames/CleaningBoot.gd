bcc1a8b83f8103ce76570429bb00311f62d1bfa1
Will Massam <wmas516@aucklanduni.ac.nz>
2026-09-02 00:29:57 +1200

    feat: added assets to clean boot minigame

--- guardians-of-the-forrest/main/scripts/src/main/minigames/CleaningBoot.gd
+++ guardians-of-the-forrest/main/scripts/src/main/minigames/CleaningBoot.gd
@@ -1,16 +1,30 @@
 extends Node2D
 
 @export var sprites: Array[Sprite2D]
+@export var cleaningRate: float = 0.01
 
 @onready var spotLabel: Label = $HUD/MarginContainer/HBoxContainer/Wave/HBoxContainer/MarginContainer2/CurrentSpots
 @onready var completion_panel: Control = $HUD/ReturnBox
 @onready var completion_button: Button = $HUD/ReturnBox/ReturnButton
+
+@onready var bootLeft: Sprite2D = $BootLeft
+@onready var dirtyLeft: Sprite2D = $BootLeft/Dirty
+@onready var bootRight: Sprite2D = $BootRight
+@onready var dirtyRight: Sprite2D = $BootRight/Dirty
+
+@onready var areaLeft: StaticBody2D = $Left
+@onready var areaRight: StaticBody2D = $Right
+
+var rightFocused: bool = true
 
 var isDragging = false
 var level_complete: bool = false
 
 func _ready():
 	spotLabel.text = str(sprites.size())
+	areaLeft.visible = true
+	areaRight.visible = true
+	swapFoot()
 
 func _input(event):
 	if level_complete:
@@ -34,8 +48,8 @@ func _input(event):
 func dragSprite(curSprite):
 	if level_complete:
 		return
-	if curSprite.modulate.a > 0.1:
-		curSprite.modulate.a = curSprite.modulate.a - 0.01
+	if curSprite.modulate.a > 0.05:
+		curSprite.modulate.a = curSprite.modulate.a - cleaningRate
 	else:
 		curSprite.modulate.a = 0
 		sprites.erase(curSprite)
@@ -59,7 +73,7 @@ func isPointInsideSprite(globalPoint: Vector2) -> Sprite2D:
 			curSpriteRect = Rect2(-textureSize / 2, textureSize)
 		else:
 			curSpriteRect = Rect2(Vector2.ZERO, textureSize)
-		if curSpriteRect.has_point(localPos):
+		if curSprite.visible == true && curSpriteRect.has_point(localPos):
 			return curSprite
 	return null
 
@@ -73,3 +87,27 @@ func miniGameOver():
 
 func _on_return_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main/scenes/levels/platforming/Platforming.tscn")
+
+
+func _on_shoe_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	var mouse_event := event as InputEventMouseButton
+	if (mouse_event 
+	and mouse_event.button_index == MOUSE_BUTTON_LEFT 
+	and mouse_event.pressed
+	and (rightFocused && !dirtyRight
+	or  !rightFocused && !dirtyLeft
+	)):
+		swapFoot()
+
+
+func swapFoot():
+	rightFocused = !rightFocused
+	bootLeft.visible = !rightFocused
+	if (dirtyLeft):
+		dirtyLeft.visible = !rightFocused
+	areaRight.visible = !rightFocused
+	
+	areaLeft.visible = rightFocused
+	if (dirtyRight):
+		dirtyRight.visible = rightFocused
+	bootRight.visible = rightFocused
