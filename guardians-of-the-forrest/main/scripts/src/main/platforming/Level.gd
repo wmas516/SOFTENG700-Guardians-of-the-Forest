270f22528465ba17632a185798d75cab937a4d17
Henry Ly <lyhenry2004@gmail.com>
2026-09-03 21:54:55 +1200

    Placed tree trim minigame interactable and connected

--- guardians-of-the-forrest/main/scripts/src/main/platforming/Level.gd
+++ guardians-of-the-forrest/main/scripts/src/main/platforming/Level.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
 @onready var player: PlatformPlayer = $Gameplay/Player
-@onready var start: Marker2D = $Markers/StartPos
+@onready var start_pos: Marker2D = $Markers/StartPos
+@onready var defense_pos: Marker2D = $Markers/DefensePos
+@onready var minigame_boot_pos: Marker2D = $Markers/MinigameBootPos
+@onready var minigame_trim_pos: Marker2D = $Markers/MinigameTrimPos
+
 @onready var defense_interactable: Interactable = $Gameplay/Interactables/DefenseInteractable
 @onready var minigame_boot_interactable: Interactable = $Gameplay/Interactables/MinigameBootInteractable
+@onready var minigame_trim_interactable: Interactable = $Gameplay/Interactables/MinigameTrimInteractable
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	defense_interactable.interacted.connect(go_to_defense)
 	minigame_boot_interactable.interacted.connect(go_to_boot_clean)
+	minigame_trim_interactable.interacted.connect(go_to_tree_trim)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,17 +25,22 @@ func _restore_player_position() -> void:
 	if PlayerData.has_saved_platforming_position:
 		player.global_position = PlayerData.saved_platforming_position
 	else:
-		player.global_position = start.global_position
+		player.global_position = start_pos.global_position
 
 func go_to_defense(_source: Interactable) -> void:
 	print("Go to defense")
-	PlayerData.save_platforming_position(player.global_position)
+	PlayerData.save_platforming_position(defense_pos.global_position)
 	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/defense/Defense.tscn")
 	
 func go_to_boot_clean(_source: Interactable) -> void:
 	print("Go to boot clean")
-	PlayerData.save_platforming_position(player.global_position)
+	PlayerData.save_platforming_position(minigame_boot_pos.global_position)
 	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/minigames/CleaningBoot.tscn")
+	
+func go_to_tree_trim(_source: Interactable) -> void:
+	print("Go to tree trim")
+	PlayerData.save_platforming_position(minigame_trim_pos.global_position)
+	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/minigames/TreeTrim.tscn")
 
 func _on_deathzone_body_entered(body: Node2D) -> void:
 	_restore_player_position()
