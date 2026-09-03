a6198233300cd6b7424f5c03e0cba0c340722c19
Henry Ly <lyhenry2004@gmail.com>
2026-09-03 20:28:53 +1200

    Placed some markers and added deathzone for forest section

--- guardians-of-the-forrest/main/scripts/src/main/platforming/Level.gd
+++ guardians-of-the-forrest/main/scripts/src/main/platforming/Level.gd
@@ -1,66 +1,24 @@
 extends Node2D
-@onready var start_position: Marker2D = $StartPosition
-@onready var tree: Interactable = $Interactables/Tree
-@onready var tree_2: Interactable = $Interactables/Tree2
-@onready var player: PlatformPlayer = $Player
-@onready var current_hp_label: Label = $UI/HUD/MarginContainer/HBoxContainer/Wave/HBoxContainer/MarginContainer2/CurrentHP
 
-var player_dead: bool = false
+@onready var player: PlatformPlayer = $Gameplay/Player
+@onready var start: Marker2D = $Markers/Start
 
-# In any parent node or level script
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	PlayerData.set_health(100)
-	tree.interacted.connect(heal_tree)
-	tree_2.interacted.connect(go_to_defense)
-	PlayerData.health_changed.connect(_on_player_health_changed)
-	PlayerData.player_died.connect(_on_player_died)
-	_update_current_hp(PlayerData.current_health, PlayerData.max_health)
-	_restore_player_position()
-	
-func go_to_defense(_source: Interactable) -> void:
-	print("Go to defense")
-	PlayerData.save_platforming_position(player.global_position)
-	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/defense/Defense.tscn")
-
-func go_to_boot_clean(_source: Interactable) -> void:
-	print("Go to boot clean")
-	PlayerData.save_platforming_position(player.global_position)
-	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/minigames/CleaningBoot.tscn")
-
-func go_to_tree_trim(_source: Interactable) -> void:
-	print("Go to tree trim")
-	PlayerData.save_platforming_position(player.global_position)
-	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/minigames/TreeTrim.tscn")
-
-func go_to_boss(_source: Interactable) -> void:
-	print("Go to tree trim")
-	PlayerData.save_platforming_position(player.global_position)
-	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/defense/Boss.tscn")
+	pass # Replace with function body.
 
 
-func heal_tree(source: Interactable) -> void:
-	var sprite: Sprite2D = source.find_child("Sprite2D")
-	sprite.texture = preload("uid://cofm40o8eagvr")
-	source.interactable_enabled = false
-	source._hide_label()
-	print("Tree Healed!")
-
-func _on_player_health_changed(new_health: int, _max_health: int) -> void:
-	if player_dead:
-		current_hp_label.text = "0"
-		return
-
-	_update_current_hp(new_health, _max_health)
-
-func _on_player_died() -> void:
-	player_dead = true
-	current_hp_label.text = "0"
-
-func _update_current_hp(new_health: int, _max_health: int) -> void:
-	current_hp_label.text = str(maxi(new_health, 0))
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
 
 func _restore_player_position() -> void:
 	if PlayerData.has_saved_platforming_position:
 		player.global_position = PlayerData.saved_platforming_position
 	else:
-		player.global_position = start_position.global_position
+		player.global_position = start.global_position
+
+
+func _on_deathzone_body_entered(body: Node2D) -> void:
+	_restore_player_position()
