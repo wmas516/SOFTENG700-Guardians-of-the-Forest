d737c8a9ec14dcebe49e18c49f71ad8bebdc27aa
Henry Ly <lyhenry2004@gmail.com>
2026-09-03 21:24:37 +1200

    Placed down defense interactable and connected

--- guardians-of-the-forrest/main/scripts/src/main/platforming/Level.gd
+++ guardians-of-the-forrest/main/scripts/src/main/platforming/Level.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 @onready var player: PlatformPlayer = $Gameplay/Player
-@onready var start: Marker2D = $Markers/Start
+@onready var start: Marker2D = $Markers/StartPos
+@onready var defense_interactable: Interactable = $Gameplay/Interactables/DefenseInteractable
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	defense_interactable.interacted.connect(go_to_defense)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,6 +19,10 @@ func _restore_player_position() -> void:
 	else:
 		player.global_position = start.global_position
 
+func go_to_defense(_source: Interactable) -> void:
+	print("Go to defense")
+	PlayerData.save_platforming_position(player.global_position)
+	get_tree().change_scene_to_file.call_deferred("res://main/scenes/levels/defense/Defense.tscn")
 
 func _on_deathzone_body_entered(body: Node2D) -> void:
 	_restore_player_position()
